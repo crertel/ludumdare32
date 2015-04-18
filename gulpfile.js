7c9b16fab4145c12c0f1507eba259c1a8fc50c0c
Chris Ertel <chris@kedagital.com>
2015-04-18 01:01:39 -0500

    Add gulpfile for asset processing.

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,30 @@ var gulp = require('gulp'),
     cache = require('gulp-cache'),
     del = require('del');
 
+gulp.task('styles', function() {
+    return gulp.src('src/styles/**/*.css')
+                                    .pipe(concat('main.css'))
+                                    .pipe(gulp.dest('static/css'))
+                                    .pipe(rename({suffix: '.min'}))
+                                    .pipe(gulp.dest('static/css'))
+                                    .pipe(notify({ message: 'Styles task complete'}));
+});
+
 gulp.task('scripts', function() {
     return gulp.src('src/scripts/**/*.js')
                                      .pipe(jshint('.jshintrc'))
                                      .pipe(jshint.reporter('default'))
                                      .pipe(concat('main.js'))
-                                     .pipe(gulp.dest('dist/assets/js'))
+                                     .pipe(gulp.dest('static/js'))
                                      .pipe(rename({suffix: '.min'}))
-                                     .pipe(gulp.dest('dist/assets/js'))
+                                     .pipe(gulp.dest('static/js'))
                                      .pipe(notify({ message: 'Scripts task complete' }));
-                                     });
+});
+
+gulp.task('clean', function(cb) {
+    del(['static/css', 'static/js'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles','scripts');
+});
